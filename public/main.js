191858b9988fcdf233b8b10f8e5aa0d4dbeea861
Michael J. Stokes <mjstokes1986@att.net>
2016-05-02 05:49:50 -0400

    checkpoint 5:49am

--- public/main.js
+++ public/main.js
@@ -7,75 +7,85 @@ $(document).ready(function(){
 
 	$('#project-link-1').click(function(){
 		window.location.href = ('https://namethattube.herokuapp.com/')
-	})
+	});
 
 	$('#source-code-1').click(function(){
 		window.location.href = ('https://github.com/MJStokes86/Name-That-Tube')
-	})
+	});
 
 	$('#project-link-2').click(function(){
 		window.location.href = ('https://stokesblog.herokuapp.com/')
-	})
+	});
 
 	$('#source-code-2').click(function(){
 		window.location.href = ('https://github.com/MJStokes86/Rails-Blog')
-	})
+	});
 
 	$('#project-link-3').click(function(){
 		window.location.href = ('http://stokes-todo-app.herokuapp.com/')
-	})
+	});
 
 	$('#source-code-3').click(function(){
 		window.location.href = ('https://github.com/MJStokes86/ToDo-App')
-	})
+	});
 
 	// $('#project-link-4').click(function(){
 	// 	window.location.href = ('http://stokes-todo-app.herokuapp.com/')
-	// })
+	// });
 
 	$('#source-code-4').click(function(){
 		window.location.href = ('https://github.com/WDI-Bulbasaur/Traveling_Football_Fan')
-	})
+	});
 
 	// $('#project-link-5').click(function(){
 	// 	window.location.href = ('')
-	// })
+	// });
 
 	// $('#source-code-5').click(function(){
 	// 	window.location.href = ('')
-	// })
+	// });
 
 	// $('#project-link-6').click(function(){
 	// 	window.location.href = ('')
-	// })
+	// });
 
 	$('#source-code-6').click(function(){
 		window.location.href = ('https://github.com/MJStokes86/Movie-Review-App')
-	})
+	});
 
 	// $('#project-link-7').click(function(){
 	// 	window.location.href = ('')
-	// })
+	// });
 
 	// $('#source-code-7').click(function(){
 	// 	window.location.href = ('')
-	// })
+	// });
 
 	$('#project-link-8').click(function(){
 		window.location.href = ('http://mjstokes86.herokuapp.com/')
-	})
+	});
 
 	$('#source-code-8').click(function(){
 		window.location.href = ('https://github.com/MJStokes86/Portfolio-Site')
-	})
+	});
 
 	// $('#project-link-9').click(function(){
 	// 	window.location.href = ('')
-	// })
+	// });
 
 	$('#source-code-9').click(function(){
 		window.location.href = ('https://github.com/MJStokes86/Portfolio-Site-II')
-	})
+	});
+
+
+
+	$('#project-link-10').click(function(){
+		window.location.href = ('http://nlm-marketing.herokuapp.com/')
+	});
+
+	$('#source-code-10').click(function(){
+		window.location.href = ('https://github.com/MJStokes86/NLM-Marketing')
+	});
 
 
 
