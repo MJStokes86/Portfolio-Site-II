ee3531e1c778e89049c1691328b7402ea1221834
Michael J. Stokes <mjstokes1986@att.net>
2016-08-08 02:00:54 -0400

    checkpoint 2:00am

--- main.rb
+++ main.rb
@@ -15,28 +15,30 @@ post '/' do
 	subject = params[:subject]
 	body  = params[:body]
 
-Pony.options = {
-  :via => :smtp,
-  :via_options => {
-    :address              => 'outbound.att.net',
-    :port                 => '465',
-    :enable_starttls_auto => true,
-    :user_name            => 'mjstokes1986@att.net',
-    :password             => 'ihave9sourcesofincome',
-    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-    :domain               => 'att.net',
-    :ssl 				  => true
-  }
-}
+
+Pony.mail({
+	:to => 'mjstokes1986@att.net',
+	:from => name +  '<' + mail + '>',
+	:reply_to => mail,
+	:subject => subject,
+	:body => body, 
+	:via => :smtp,
+	:via_options => {
+		:address              => 'outbound.att.net',
+	    :port                 => 465,
+	    :enable_starttls_auto => true,
+	    :user_name            => 'mjstokes1986@att.net',
+	    :password             => 'ihave9sourcesofincome',
+	    :authentication       => :login, # :plain, :login, :cram_md5, no auth by default
+	    :domain               => 'att.net',
+	    :ssl 				  => true
+		
+
+		
+	}
+	})
 
 
-	Pony.mail(
-		:to => 'mjstokes1986@att.net', 
-		:from => name + '<' + mail + '>',
-		:reply_to => mail,
-		:subject => subject,
-		:body => body
-		)
 		
 	
 
